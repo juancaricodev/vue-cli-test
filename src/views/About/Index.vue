87734fab22452844e9462c361ae23b59b08e923f
Juan Camilo Rico <juancarico92@gmail.com>
2021-01-17 22:23:58 -0500

    feat: trying out scss for styles

--- src/views/About/Index.vue
+++ src/views/About/Index.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <section class="about__navbar">
+      <div class="about__navbar-item"></div>
+      <div class="about__navbar-item"></div>
+      <div class="about__navbar-item"></div>
+    </section>
   </div>
 </template>
+
+<style lang="scss">
+.about {
+  width: 100%;
+  background-color: aquamarine;
+
+  &__navbar {
+    border: solid 3px blue;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+
+    &-item {
+      width: 100%;
+      height: 200px;
+      border: solid 1px red;
+      background-color: cornflowerblue;
+    }
+
+  }
+}
+</style>
