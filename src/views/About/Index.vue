768d53c0feb1ef5bd186e52438ef841503cde6cb
Juan Camilo Rico <juancarico92@gmail.com>
2021-01-17 23:27:52 -0500

    feat(about): trying out template element state

--- src/views/About/Index.vue
+++ src/views/About/Index.vue
@@ -2,12 +2,46 @@
   <div class="about">
     <h1>This is an about page</h1>
     <section class="about__navbar">
-      <div class="about__navbar-item"></div>
+      <div
+        class="about__navbar-item"
+        :style="{ backgroundColor: bgColor }"
+        @click="changeColor"
+      ></div>
       <div class="about__navbar-item"></div>
       <div class="about__navbar-item"></div>
     </section>
+    <!-- <button class="about__btn" @click="changeColor">Change Color</button> -->
   </div>
 </template>
+
+<script>
+import { reactive, toRefs, computed } from 'vue'
+export default {
+  setup () {
+    const aboutState = reactive({
+      colorActive: false,
+
+      bgColor: computed(() => {
+        if (aboutState.colorActive === false) {
+          return 'papayawhip'
+        } else if (aboutState.colorActive === true) {
+          return 'cornflowerblue'
+        }
+      })
+    })
+
+    const changeColor = () => {
+      aboutState.colorActive = !aboutState.colorActive
+    }
+
+    return {
+      ...toRefs(aboutState),
+
+      changeColor
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 .about {
@@ -23,9 +57,12 @@
       width: 100%;
       height: 200px;
       border: solid 1px red;
-      background-color: cornflowerblue;
+      /* background-color: cornflowerblue; */
     }
-
   }
 }
+
+/* .active {
+  background: papayawhip;
+} */
 </style>
